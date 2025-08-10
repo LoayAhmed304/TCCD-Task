e62a9b874bbea338dd9b181a54e238f9f9d795fa
LoayAhmed304 <loayahmed304@gmail.com>
2025-08-10 17:24:50 +0300

    feat(db): create schema.sql for easier database initialization

---
+++ sql/schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE ecommerce_db;
+
+\c ecommerce_db;
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    description TEXT,
+    UNIQUE(name)
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    description TEXT,
+    price NUMERIC(10, 2) NOT NULL,
+    stock INT DEFAULT 0,
+    category_id INT REFERENCES categories(id) ON DELETE SET NULL,
+    UNIQUE(name)
+);
+
+CREATE TABLE carts (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    UNIQUE(user_id)
+);
+
+CREATE TABLE cart_items (
+    id SERIAL PRIMARY KEY,
+    cart_id INT REFERENCES carts(id) ON DELETE CASCADE,
+    product_id INT REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    UNIQUE (cart_id, product_id)
+);
